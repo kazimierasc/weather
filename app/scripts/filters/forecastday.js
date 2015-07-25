e446d3e4dd97053cdc76af363a12390a5c9145bb
Kazimieras <kazimieras@ide.lt>
2015-07-25 14:48:11 +0300

    Bugfix: wrong weekday was being displayed

--- app/scripts/filters/forecastday.js
+++ app/scripts/filters/forecastday.js
@@ -11,7 +11,7 @@
 angular.module('weatherApp')
   .filter('forecastDay', function () {
     return function (input) {
-    	var daysOfWeek = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+    	var daysOfWeek = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
     	return daysOfWeek[input.getDay()]+' '+(input.getMonth()+1)+'-'+(input.getDate());
     };
   });
