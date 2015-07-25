956ddc07a67d56cdb59f42ab9d2ff89aaefc17dd
Kazimieras <kazimieras@ide.lt>
2015-07-25 21:15:23 +0300

    Factored the search controller into the header controller and made the starred list more responsive

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -8,12 +8,19 @@
  * Controller of the weatherApp
  */
 angular.module('weatherApp')
-  .controller('HeaderCtrl', ['$scope','requestprocessor',function ($scope,requestprocessor) {
+  .controller('HeaderCtrl', ['$scope','requestprocessor','$location',function ($scope,requestprocessor,$location) {
     $scope.visibility = false;
     $scope.toggleMenu = function() {
     	$scope.visibility = !$scope.visibility;
     };
     $scope.coordinatesSeek = function() {
     	requestprocessor.deviceLocationLoad();
+    };
+    $scope.keyword = '';
+    $scope.performSearch = function() {
+    	$scope.toggleMenu();
+    	if($scope.keyword.length !== 0) {
+    		$location.path('/'+$scope.keyword);
+    	}
     };
   }]);
